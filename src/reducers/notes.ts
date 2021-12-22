b7ceeca9adfb56ba3602636bc8ea02fcaab0f74f
Giwoo G Lee <hazel@Giwoo-Lee.local>
2021-12-22 16:20:49 +0900

    feat: Include first reducer with typescript

---
+++ src/reducers/notes.ts
@@ -0,0 +1,11 @@
+import { Note, FetchNotesAction } from '../actions';
+import { ActionTypes } from '../actions/types';
+
+export const notesReducer = (state: Note[] = [], action: FetchNotesAction) => {
+    switch(action.type) {
+        case ActionTypes.fetchNotes:
+            return action.payload;
+        default:
+            return state;
+    }
+} 
