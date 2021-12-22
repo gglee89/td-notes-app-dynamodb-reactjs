7b2de9149aa2f735fa636c6a82a4ed8a6e6e617e
Giwoo G Lee <gglee89@gmail.com>
2021-12-22 17:51:52 +0900

    feat: add deleteNote action integrated to App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Note, fetchNotes } from '../actions';
+import { Note, fetchNotes, deleteNote } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
     notes: Note[];
-    fetchNotes(): any;
-
+    fetchNotes: Function;
+    deleteNote: Function;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+    fetching: boolean
+}
+
+class _App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props)
+
+        this.state = {
+            fetching: false
+        }
+    }
+
+    componentDidUpdate(prevProps: AppProps) {
+        if (!prevProps.notes.length && this.props.notes.length) {
+            this.setState({ fetching: false })
+        }
+    }
+
     onButtonClick = (): void => {
         this.props.fetchNotes()
+        this.setState({
+            fetching: true
+        })
+    }
+
+    onNoteClick = (timestamp: number): void => {
+        this.props.deleteNote(timestamp)
     }
 
     renderList = (): JSX.Element[] => {
         return this.props.notes.map((note: Note) => {
-            return <div key={note.timestamp}>{note.title}</div>
+            return <div onClick={() => this.onNoteClick(note.timestamp)} key={note.timestamp}>{`${note.title} | ${note.timestamp}`}</div>
         })
     }
 
@@ -24,6 +49,7 @@ class _App extends React.Component<AppProps> {
         return (
             <div>
                 <button onClick={this.onButtonClick}>Fetch Notes</button>
+                {this.state.fetching && 'LOADING...'}
                 {this.renderList()}
             </div>
         )
@@ -36,4 +62,4 @@ const mapStateToProps = (state: StoreState): { notes: Note[] } => {
     }
 }
 
-export const App = connect(mapStateToProps, { fetchNotes })(_App)
+export const App = connect(mapStateToProps, { fetchNotes, deleteNote })(_App)
--- src/reducers/notes.ts
+++ src/reducers/notes.ts
@@ -1,10 +1,14 @@
-import { Note, FetchNotesAction } from '../actions';
-import { ActionTypes } from '../actions/types';
+import { Note, Action, ActionTypes } from '../actions';
 
-export const notesReducer = (state: Note[] = [], action: FetchNotesAction) => {
+export const notesReducer = (
+    state: Note[] = [], 
+    action: Action
+) => {
     switch(action.type) {
         case ActionTypes.fetchNotes:
             return action.payload;
+        case ActionTypes.deleteNote:
+            return state.filter((note: Note) => note.timestamp !== action.payload)
         default:
             return state;
     }
