e312776b946375c9fa44f0aa29e557768dbdb3a8
Giwoo G Lee <hazel@Giwoo-Lee.local>
2021-12-23 23:49:22 +0900

    feat: Prettier + NotesApiService

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,37 +1,112 @@
 import React from 'react';
 import {
-    Avatar,
-    Box, Container, CssBaseline, Typography
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography
 } from '@mui/material';
-import {
-    LockOutlined as LockOutlinedIcon
-} from '@mui/icons-material';
+import { LockOutlined as LockOutlinedIcon } from '@mui/icons-material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme();
-const Login = () => {
+const Copyright = (props: any) => {
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>                    
-                </Box>
-            </Container>
-        </ThemeProvider>
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com">
+                Genealogy Care App
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
     )
 }
 
-export default Login
+const theme = createTheme();
+const Login = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              name="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel 
+                control={<Checkbox value="remember" color="primary" />} 
+                label="Remember me"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Sign In
+            </Button>
+            <Grid container>
+                <Grid item xs>
+                    <Link href="#" variant="body2">
+                        Forgot password?
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Link href="#" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
+                </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default Login;
---
+++ src/services/auth-guard/README.md
@@ -0,0 +1,2 @@
+It's a service that checks IF the user is logged in or not.  
+And if the user tries to access the app without logging in, it will redirect the user back to the login page.
